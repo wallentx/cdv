afc4273a84ffdab10a4a1d98bef061bdb9bb052e
wallentx <william.allentx@gmail.com>
2023-01-25 15:00:23 -0600

    POC structure

--- activated.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$script_directory = Split-Path $MyInvocation.MyCommand.Path -Parent
-if (Test-Path -Path $script_directory/venv) {
-    & $script_directory/venv/Scripts/Activate.ps1
-   }
-& @args
-
-exit $LASTEXITCODE
